b0f79103a4294e29ee20b93ed19cafcca4375437
Kalinka George <xitowzys@gmail.com>
2023-11-12 23:23:51 +0800

    ✨ feat: Creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './components/pages/Menu/Menu';
+import { Cart } from './components/pages/Cart/Cart';
+import { Error } from './components/pages/Error/Error';
 
 function App() {
 
@@ -15,6 +19,14 @@ function App() {
             <Button onClick={addCounter}>Кнопка</Button>
             <Button appearence='big' onClick={addCounter}>Кнопка</Button>
             <Input placeholder='Email' />
+
+
+
+            <Routes>
+                <Route path='/' element={<Menu />}/>
+                <Route path='/cart' element={<Cart />}/>
+                <Route path='*' element={<Error />}/>
+            </Routes>
         </>
     );
 }
