7438babbb5861e5a037150040b590d87325fa037
Kalinka George <xitowzys@gmail.com>
2023-11-14 23:04:52 +0800

    ✨ feat: Creating requests via fetch

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,8 +2,36 @@ import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 import ProductCard from '../../components/ProductCard/ProductCard';
+import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
+import { useEffect, useState } from 'react';
 
 export function Menu() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    const getMenu = async () => {
+
+        try {
+            const res = await fetch(`${PREFIX}/products`);
+
+            if (!res.ok) {
+                return;
+            }
+    
+            const data: Product[] = await res.json() as Product[];
+    
+            setProducts(data);
+        } catch(e) {
+            console.error(e);
+            return;
+        }
+    };
+
+    useEffect(() => {
+        getMenu();
+    }, []);
+
     return (
         <>
             <div className={styles['head']}>
@@ -12,14 +40,18 @@ export function Menu() {
             </div>
 
             <div>
-                <ProductCard
-                    id={1}
-                    title='Наслаждение'
-                    description='Салями, руккола, помидоры, оливки'
-                    rating={4.5}
-                    price={300}
-                    image='/product-demo.png'
-                />
+
+                {products.map(p => (
+                    <ProductCard
+                        key={p.id}
+                        id={p.id}
+                        title={p.name}
+                        description={p.ingredients.join(', ')}
+                        rating={p.rating}
+                        price={p.price}
+                        image={p.image}
+                    />
+                ))}
             </div>
         </>
     );
